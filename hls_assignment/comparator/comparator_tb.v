ed9d039a509e1da71135e3f2d5654a8ba864cc29
Munees-Sanid <shanidshaan192@gmail.com>
2025-04-14 12:56:04 +0530

    update

---
+++ hls_assignment/comparator/comparator_tb.v
@@ -0,0 +1,31 @@
+`include "comparator.v"
+
+module comparator_tb;
+reg [3:0] a, b;
+wire a_gt_b, a_eq_b, a_ls_b;
+
+comparator dut (.*); // It automatically port the connection 
+
+// To display the value
+initial begin
+$monitor("Time = %0t  a = %b  b = %b   ::   a_gt_b = %b a_eq_b = %b  a_ls_b = %b",$time, a, b, a_gt_b, a_eq_b, a_ls_b);
+end
+// few combination of 4 bit a and b 
+initial begin
+ a = 4'b0000; b = 4'b0000;
+ #10 a = 4'b0001; b = 4'b0000;
+ #10 a = 4'b0010; b = 4'b0011;
+ #10 a = 4'b0101; b = 4'b0101;
+ #10 a = 4'b0111; b = 4'b0110;
+ #10 a = 4'b1000; b = 4'b1000;
+ #10 a = 4'b1100; b = 4'b1101;
+ #10 a = 4'b1111; b = 4'b0001;
+ #10 a = 4'b0000; b = 4'b1111;
+end
+
+initial begin
+ $dumpfile("dump.vcd");
+ $dumpvars(0, comparator_tb);
+end
+endmodule
+
