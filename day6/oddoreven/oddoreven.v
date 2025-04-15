9d8456b95ad868bd20b460d0fb4a153889e0eb87
Munees-Sanid <shanidshaan192@gmail.com>
2025-04-15 23:13:10 +0530

    update

--- day6/oddoreven/oddoreven.v
+++ day6/oddoreven/oddoreven.v
@@ -1,4 +1,4 @@
-module pallindrome(
+module oddoreven(
         input [15:0]a,
         output [8*4:0]y);
 assign y = ((a[0])==1'b0)?"even":"odd";
@@ -7,7 +7,7 @@ endmodule
 module tb;
 reg [15:0]a;
 wire [8*4:0]y ;
-pallindrome dut (.*);
+oddoreven dut (.*);
 initial begin
 a=16'd3;
 #5 a=16'd2;
