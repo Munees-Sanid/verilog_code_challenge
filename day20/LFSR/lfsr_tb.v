d73ef5337ff8263ccc78d595654c7ca2e6377e9a
Munees-Sanid <shanidshaan192@gmail.com>
2025-04-26 19:09:36 +0530

    update

--- day20/LFSR/lfsr_tb.v
+++ day20/LFSR/lfsr_tb.v
@@ -3,8 +3,7 @@
 module tb;
 wire clk;
 wire [7:0]q;
-integer t_count=0;
-reg [7:0] q1=0;
+reg [7:0] count=0;
 initial begin
     force tb.dut.q = 4'b0001; 
     #5 release tb.dut.q;       
@@ -13,13 +12,11 @@ lfsr dut(clk,q);
 clock c1(clk);
 
 always @(posedge clk) begin
-if (q != q1) begin
-t_count = t_count + 1;
-q1 = q;
+count =count+1;
  end
- end
+ 
 initial begin 
-$monitor("%d :: q = %b",t_count,q);
+$monitor("%d :: q = %b",count,q);
 end
 initial begin 
 $dumpfile("dump.vcd");
