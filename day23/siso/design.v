c9d1ba54d620e16f1b5e9bb51c5f81813a2983f5
Munees-Sanid <shanidshaan192@gmail.com>
2025-04-29 11:41:32 +0530

    update

---
+++ day23/siso/design.v
@@ -0,0 +1,15 @@
+// design code for serial in serial out
+
+module siso(
+	input clk,rst,s_in,
+	output reg s_out);
+reg [3:0]shift_register;
+always@(posedge clk or posedge rst)begin
+	if(rst)
+		shift_register<=0;
+	else 
+		shift_register<={shift_register[2:0],s_in};
+	end
+assign s_out = shift_register[7];
+endmodule
+
