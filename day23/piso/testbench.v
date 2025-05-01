3c70e71dfbae5ba2fdbdbd905d1d1ca1d07279a9
Munees-Sanid <shanidshaan192@gmail.com>
2025-05-01 21:07:38 +0530

    update

--- day23/piso/testbench.v
+++ day23/piso/testbench.v
@@ -4,10 +4,12 @@ module tb;
 reg rst,clk,load;
 reg [3:0]parallel_in;
 wire s_out;
+integer file;
 piso dut(rst,clk,load,parallel_in,s_out);
 always #5 clk=~clk;
 initial begin
-$monitor("sim= %0t rst =%b load=%b :: parrarel in =%d :: shift register =%b :: serial out =%b",$time,rst,load,parallel_in,dut.shift_register,s_out);
+file = $fopen("output_data.txt", "w");
+
 clk=0;
 rst=1;
 // reset after 10s
@@ -16,7 +18,11 @@ rst=1;
 //  data value
 #5 load=1; parallel_in=4'b1010;
 #5 load=0;
-#50 $finish;
+#50 $fclose(file);
+ $finish;
+end
+always@(posedge clk)begin
+$fdisplay(file,"sim= %0t rst =%b load=%b :: parrarel in =%d :: shift register =%b :: serial out =%b",$time,rst,load,parallel_in,dut.shift_register,s_out);
 end
 initial begin 
 $dumpfile("dump.vcd");
