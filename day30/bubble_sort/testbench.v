5dfd5bddbada39c48af8d23a7850407f4cf3526f
Munees-Sanid <shanidshaan192@gmail.com>
2025-05-08 19:43:25 +0530

    update

---
+++ day30/bubble_sort/testbench.v
@@ -0,0 +1,26 @@
+module tb;
+  integer arr[0:4];
+  integer i, j, temp;
+
+  initial begin
+  arr[0]=3;
+  arr[1]=5;
+  arr[2]=2;
+  arr[3]=1;
+    for (i = 0; i < 5; i = i + 1) begin
+      for (j = 0; j < 5; j = j + 1) begin
+        if (arr[j] > arr[j + 1]) begin
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        end
+      end
+    end
+
+    $display("Sorted array:");
+    for (i = 0; i < 5; i = i + 1)
+       $display("%0d ", arr[i]);
+   
+  end
+endmodule
+
