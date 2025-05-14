50dac43ca26ed4b47dc33b872dbc1092f4435c7b
Munees-Sanid <shanidshaan192@gmail.com>
2025-05-14 13:05:15 +0530

    update

---
+++ day32/counting_0_2_3_7_using_fsm/design.v
@@ -0,0 +1,34 @@
+// counting 0 2 3 7 0 reapeat
+module counter(
+	input clk,rst,
+	output reg [3:0]count);
+
+reg [2:0] ps,ns;
+
+parameter s0=3'b000;
+parameter s2=3'b010;
+parameter s3=3'b011;
+parameter s7=3'b111;
+
+always@(posedge clk)
+begin 
+	if(~rst)
+		ps<=s0;
+	else
+		ps<=ns;
+end
+
+always @(ps)begin
+case(ps)
+s0: ns=s2;
+s2: ns=s3;
+s3: ns=s7;
+s7: ns=s0;
+
+
+endcase
+end
+always@(*)begin
+count=ps;
+end
+endmodule
---
+++ day32/counting_0_2_3_7_using_fsm/testbench.v
@@ -0,0 +1,20 @@
+`include"design.v"
+// test bench
+module tb;
+reg clk,rst;
+wire [3:0]count;
+counter dut(.*);
+initial begin
+$monitor("time = %0t  count = %d",$time,count);
+clk=0;
+rst=0;
+#12 rst=1;
+
+#120 $finish;
+end
+always #5 clk =~clk;
+initial begin 
+$dumpfile("dump.vcd");
+$dumpvars(0,tb);
+end
+endmodule
