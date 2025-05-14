35e2eaae0b8c9a2468db9a234b49ad343e411d7a
Munees-Sanid <shanidshaan192@gmail.com>
2025-05-14 10:49:50 +0530

    update

---
+++ day32/mealy_10110_overlapping/design.v
@@ -0,0 +1,67 @@
+// mealy overlapping 10110
+module mealy(
+	input in_seq,clk,rst,
+	output reg out);
+reg [2:0]ps,ns;
+parameter idle = 3'b000;
+parameter s1 = 3'b001;
+parameter s10 = 3'b010;
+parameter s101 = 3'b011;
+parameter s1011 = 3'b100;
+parameter s10110 = 3'b101;
+
+always@(posedge clk)
+	begin
+	if(!rst)
+		ps<=idle;
+	else 
+		ps<=ns;
+	end
+
+always@(in_seq,ps)begin
+case(ps)
+	idle: if(in_seq)begin
+		ns=s1;
+		out=0;
+		end
+		else begin
+		ns=idle;
+		out=0;
+		end
+		
+	s1: if(in_seq)begin
+		ns=s1;
+		out=0;
+		end
+		else begin
+		ns=s10;
+		out=0;
+		end
+	s10: if(in_seq)begin
+		ns=s101;
+		out=0;
+		end
+		else begin
+		ns=idle;
+		out=0;
+		end
+	s101: if(in_seq)begin
+		ns=s1011;
+		out=0;
+		end
+		else begin
+		ns=s1;
+		out=0;
+		end
+	s1011: if(in_seq)begin
+		ns=s1;
+		out=0;
+		end
+		else begin
+		ns=s10;
+		out=1;
+		end
+	
+	endcase
+	end
+endmodule
---
+++ day32/mealy_10110_overlapping/testbench.v
@@ -0,0 +1,30 @@
+`include"design.v"
+// test bench
+module tb;
+reg in_seq,clk,rst;
+wire out;
+mealy dut(.*);
+initial begin
+$monitor("time = %0t in_sequence = %b out = %b",$time,in_seq,out);
+clk=0;
+rst=0;
+#12 rst=1;
+in_seq=1;
+#10 in_seq=0;
+#10 in_seq=1;
+#10 in_seq=1;
+#10 in_seq=0;
+#10 in_seq=1;
+#10 in_seq=0;
+#10 in_seq=1;
+#10 in_seq=1;
+#10 in_seq=0;
+
+#20 $finish;
+end
+always #5 clk =~clk;
+initial begin 
+$dumpfile("dump.vcd");
+$dumpvars(0,tb);
+end
+endmodule
